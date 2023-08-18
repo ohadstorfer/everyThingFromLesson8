5f3223e9faafa0b15c69e14cd8718031123b12e0
ohadstorfer <ohad.storfer@gmail.com>
2023-08-18 19:18:55 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# everyThingFromLesson8" 
---
+++ app.py
@@ -0,0 +1,52 @@
+# for num in range(1,21):
+#     print(num)
+
+
+# for num in range(0,10):
+#     print("ohad")
+
+# n1 = input ("enter a number: ")
+# print (f"the number is: {int(n1) *2}")
+
+
+# n1 = input ("enter a number: ")
+# n2 = input ("enter another number: ")
+# if n1>n1: 
+#      print (n1)
+# else:
+#       print (n2)
+
+# n1 = input ("enter a number: ")
+# for num in range(1,int(n1)):
+#      print(num)
+
+# for num in range(1,21):
+#     if num%2==0:
+#        print(num)
+
+# if __name__ ==  '__main__':
+   
+# import datetime
+# current_time = datetime.datetime.now()
+# hour = current_time.hour
+# minutes = current_time.minute
+# print("Current time:", f"{hour:02d}:{minutes:02d}")
+
+
+# n1 = input ("enter your name: ")
+# print ("welcome to berlin, "+n1+"!")
+
+
+def find_larger_number(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Call the function and print the result
+larger_number = find_larger_number(num1, num2)
+print("The larger number is:", larger_number)
